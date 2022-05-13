4290089aea49c1f9f7c2424fbd18ce5887757ceb
szyba0102 <wszybinska@student.agh.edu.pl>
2022-05-13 20:37:51 +0200

    First

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#elo
+
+import cv2 as cv
+import numpy as np
+cap = cv.VideoCapture('exp.mp4')
+while cap.isOpened():
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
